5ab9de1b1c815b6c963026542dcc06a29f212f53
terrill11 <terrill.lim@gmail.com>
2020-08-07 13:59:11 -0400

    yahoo scraper class

---
+++ scripts/equities/yahoo_scraper.py
@@ -0,0 +1,33 @@
+#!/Users/terrill/OneDrive/Documents/work/projects/spy/venv_spy/bin/python
+
+from datetime import datetime
+from datetime import timedelta
+import pandas_datareader.data as web
+import requests
+
+class Yahoo():
+    def __init__(self):
+        self.source = 'yahoo'
+
+    def get_all_data(self, ticker, start, end):
+        start = datetime(1990,1,1)
+        end = (datetime.now() - timedelta(days = 1))
+        df = web.DataReader(ticker, self.source, start, end)
+        return df
+
+    def get_segment_data(self, ticker, start, end):
+        start = datetime.strptime(start, '%Y-%m-%d').date()
+        end = datetime.strptime(end, '%Y-%m-%d').date()
+        df = web.DataReader(ticker, self.source, start, end)
+        return df
+
+    def get_last_x_days_data(self, ticker, latest_entry, days_to_look_back):
+        start = datetime.now() - timedelta(days = days_to_look_back)
+        end = (datetime.now() - timedelta(days = 1))
+        df = web.DataReader(ticker, self.source, start, end)
+        df = df.loc[df.index > latest_entry]
+        return df
+
+# yahoo = Yahoo()
+# print(yahoo.get_data('2020-01-01', '2020-01-10'))
+# print(yahoo.get_last_x_days_data('MSFT', '2020-08-01', 40))
